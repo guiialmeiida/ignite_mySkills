24f4b90d97fec7f3ea0bd52d3f001ff4cad0ee2f
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-21 07:32:20 -0300

    Componentes customizados e props

---
+++ src/components/SkillCard.js
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
+export function SkillCard({ skill }) {
+    return (
+        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+                {skill}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonSkill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    textSkill: {
+        color: '#FFF',
+        fontSize: 22,
+        fontWeight: 'bold',
+
+    },
+});
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,10 @@ import {
     StyleSheet,
     Text,
     TextInput,
-    TouchableOpacity,
     View
 } from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
 
 export function Home() {
     const [newSkill, setNewSkill] = useState('');
@@ -30,13 +31,8 @@ export function Home() {
                 onChangeText={setNewSkill}
             />
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.7}
-                onPress={handleAddNewSkill}
-            >
-                <Text style={styles.buttonText}>Add</Text>
-            </TouchableOpacity>
+            <Button onPress={handleAddNewSkill} />
+
 
             <Text style={[styles.title, { marginVertical: 50 }]}>
                 My Skills
@@ -44,11 +40,7 @@ export function Home() {
 
             {
                 mySkills.map(skill => (
-                    <TouchableOpacity key={skill} style={styles.buttonSkill}>
-                        <Text style={styles.textSkill}>
-                            {skill}
-                        </Text>
-                    </TouchableOpacity>
+                    <SkillCard skill={skill} />
                 ))
             }
         </View>
@@ -74,30 +66,5 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7,
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold',
-    },
-    buttonSkill: {
-        backgroundColor: '#1F1E25',
-        padding: 15,
-        borderRadius: 50,
-        alignItems: 'center',
-        marginVertical: 10,
-    },
-    textSkill: {
-        color: '#FFF',
-        fontSize: 22,
-        fontWeight: 'bold',
-
     },
 });
